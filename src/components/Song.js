f48d03e49a6d932fd40956ed79236016c5f8443d
Timothy Saudners <timsaunders88@gmail.com>
2020-10-13 20:28:56 +0100

    added specific content to Song.js

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,8 +4,10 @@ export default function Song(props){
 
     return (
         <div>
+            <img src={props.song["im:image"][0].label} alt={"image for song: " + props.song.title.label}/>
             <h4>{props.song["im:name"].label}</h4>
             <p>Artist: {props.song["im:artist"].label}</p>
+            <p>Genre: {props.song.category.attributes.label}</p>
         </div>
     )
 
